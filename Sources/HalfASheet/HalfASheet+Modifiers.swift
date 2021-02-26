c61d63e39597d83d19b817579e2940137955200d
Sea Son Chan <ferrick90@gmail.com>
2021-02-27 00:27:52 +0800

    fix the height issue

--- Sources/HalfASheet/HalfASheet+Modifiers.swift
+++ Sources/HalfASheet/HalfASheet+Modifiers.swift
@@ -59,7 +59,7 @@ extension View {
     ///   - title: an optional title for the sheet
     ///   - content: the sheet's content
     public func halfASheet<T: View>(isPresented: Binding<Bool>, title: String? = nil, height: HalfASheetHeight = .proportional(0.84), @ViewBuilder content: @escaping () -> T) -> some View {
-        modifier(HalfASheetPresentationModifier(content: { HalfASheet(isPresented: isPresented, title: title, content: content, height: height) }))
+        modifier(HalfASheetPresentationModifier(content: { HalfASheet(isPresented: isPresented, title: title, content: content).height(height) }))
     }
 }
 
